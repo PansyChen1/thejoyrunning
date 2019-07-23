0afa8c6493283f8285f57d58f9ae2c187e322fef
chenping <chenping@wecash.net>
2019-07-23 18:41:11 +0800

    add canvas

---
+++ js/canvas.js
@@ -0,0 +1,28 @@
+Zepto(function ($) {
+  var canvas = document.getElementById('tutorial');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect (30, 30, 55, 50);
+
+    ctx.beginPath();
+    ctx.moveTo(175,200);
+    ctx.lineTo(175,240);
+    ctx.lineTo(135,240);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(75,25);
+    ctx.quadraticCurveTo(25,25,25,62.5);
+    ctx.quadraticCurveTo(25,100,50,100);
+    ctx.quadraticCurveTo(50,120,30,125);
+    ctx.quadraticCurveTo(60,120,65,100);
+    ctx.quadraticCurveTo(125,100,125,62.5);
+    ctx.quadraticCurveTo(125,25,75,25);
+    ctx.stroke();
+  }
+});
